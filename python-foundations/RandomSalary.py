fe322d0131c209a991bf8ed9e13ef1f70e0ca83b
Sai Anoop Avunuri <saianoop9@gmail.com>
2019-06-26 11:03:04 -0400

    Added Salary Function

---
+++ python-foundations/RandomSalary.py
@@ -0,0 +1,9 @@
+import random
+name = input("Enter your name: ")
+salary = int(input("Enter your salary: "))
+raise_per = random.randint(1,99)
+raise_amount = salary+raise_per*salary/100
+print(f"{name}, your current salary is ${salary}")
+print(f"Your raise is {raise_per}% of ${salary}")
+print(f"{name}, your new salary is ${raise_amount}")
+
